47badb02461e3c34d818c6c6fc8008a8e16b450f
EternalDuelist <davcarranza@gmail.com>
2016-04-15 21:17:42 -0700

    toying around with displays

--- src/JustaVidGame.java
+++ src/JustaVidGame.java
@@ -6,33 +6,25 @@ import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.*;
  
-public class YourFirstDisplay {
- 
+public class JustaVidGame {
     // We need to strongly reference callback instances.
     private GLFWErrorCallback errorCallback;
     private GLFWKeyCallback   keyCallback;
- 
-    // The window handle
-    private long window;
+    private long window;	// The window handle
  
     public void run() {
-        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
- 
         try {
             init();
             loop();
- 
-            // Destroy window and window callbacks
-            glfwDestroyWindow(window);
-            //keyCallback.free();
+            glfwDestroyWindow(window);	// Destroy window and window callbacks
         } finally {
-            // Terminate GLFW and free the GLFWErrorCallback
-            glfwTerminate();
-            //errorCallback.free();
+            glfwTerminate();			// Terminate GLFW and free the GLFWErrorCallback
         }
     }
  
     private void init() {
+    	int WIDTH = 640, HEIGHT = 480;
+    	
         // Setup an error callback. The default implementation
         // will print the error message in System.err.
         glfwSetErrorCallback(errorCallback = GLFWErrorCallback.createPrint(System.err));
@@ -45,12 +37,9 @@ public class YourFirstDisplay {
         glfwDefaultWindowHints(); // optional, the current window hints are already the default
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
- 
-        int WIDTH = 300;
-        int HEIGHT = 300;
- 
+
         // Create the window
-        window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World!", NULL, NULL);
+        window = glfwCreateWindow(WIDTH, HEIGHT, "JustaVid Game", NULL, NULL);
         if ( window == NULL )
             throw new RuntimeException("Failed to create the GLFW window");
  
@@ -65,20 +54,14 @@ public class YourFirstDisplay {
  
         // Get the resolution of the primary monitor
         GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
         // Center our window
-        glfwSetWindowPos(
-            window,
-            (vidmode.width() - WIDTH) / 2,
-            (vidmode.height() - HEIGHT) / 2
-        );
- 
-        // Make the OpenGL context current
-        glfwMakeContextCurrent(window);
-        // Enable v-sync
-        glfwSwapInterval(1);
- 
-        // Make the window visible
-        glfwShowWindow(window);
+        glfwSetWindowPos(window, (vidmode.width() - WIDTH) / 2, (vidmode.height() - HEIGHT) / 2);
+
+        glfwMakeContextCurrent(window);		// Make the OpenGL context current
+
+        glfwSwapInterval(1);				// Enable v-sync. Also sets the frame rate to 60fps?
+        glfwShowWindow(window);				// Make the window visible
     }
  
     private void loop() {
@@ -88,25 +71,20 @@ public class YourFirstDisplay {
         // creates the GLCapabilities instance and makes the OpenGL
         // bindings available for use.
         GL.createCapabilities();
- 
-        // Set the clear color
-        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f); // Set the clear color
  
         // Run the rendering loop until the user has attempted to close
         // the window or has pressed the ESCAPE key.
         while ( glfwWindowShouldClose(window) == GLFW_FALSE ) {
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);		// clear the framebuffer
  
-            glfwSwapBuffers(window); // swap the color buffers
- 
-            // Poll for window events. The key callback above will only be
-            // invoked during this call.
-            glfwPollEvents();
+            glfwSwapBuffers(window);								// draws on the screen at a frame rate specified in init
+            glfwPollEvents();										// checks for user input and takes an action based on the key callback
         }
     }
  
     public static void main(String[] args) {
-        new YourFirstDisplay().run();
+        new JustaVidGame().run();
     }
- 
 }
